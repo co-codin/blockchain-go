a5e5792d972a27fd7fc08f2a039c0a0785b05d47
elijah <cuiyeqing960904@gmail.com>
2025-11-13 20:52:52 +0300

    blockchain add block , init block

---
+++ blockchain/proof.go
@@ -0,0 +1,43 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+)
+
+type BlockChain struct {
+	blocks []*Block
+}
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+	Nonce    int
+}
+
+func (b *Block) DeriveHash() []byte {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash, 0}
+	block.Hash = block.DeriveHash()
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
