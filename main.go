df652247526d7fb7c9901887c17c26b9b10c6867
elijah <cuiyeqing960904@gmail.com>
2025-11-15 19:08:00 +0300

    integrating with badger

--- main.go
+++ main.go
@@ -7,21 +7,21 @@ import (
 )
 
 func main() {
-	chain := blockchain.InitBlockChain()
+	// chain := blockchain.InitBlockChain()
 
-	chain.AddBlock("First Block after Genesis")
-	chain.AddBlock("Second Block after Genesis")
-	chain.AddBlock("Third Block after Genesis")
+	// chain.AddBlock("First Block after Genesis")
+	// chain.AddBlock("Second Block after Genesis")
+	// chain.AddBlock("Third Block after Genesis")
 
-	for _, block := range chain.Blocks {
+	// for _, block := range chain.Blocks {
 
-		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
-		fmt.Printf("Data in Block: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
+	// 	fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+	// 	fmt.Printf("Data in Block: %s\n", block.Data)
+	// 	fmt.Printf("Hash: %x\n", block.Hash)
 
-		pow := blockchain.NewProof(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
+	// 	pow := blockchain.NewProof(block)
+	// 	fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+	// 	fmt.Println()
 
-	}
+	// }
 }
